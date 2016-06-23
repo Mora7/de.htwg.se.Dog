673d02a87347e5ab1d13a76ed276826056d74efd
kebroger <kbroger@sunrise.ch>
2016-06-24 01:19:14 +0200

    rename

---
+++ src/main/java/de/htwg/dog/model/impl/Draw.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.htwg.dog.model.impl;
+
+/**
+ *
+ * @author kev
+ */
+public class Draw {
+
+    private Draw() {
+    }
+    
+    public static boolean isDrawAllowed(Square from, Square to, ValueEnum card, Player player) {
+
+        boolean validDraw = false;
+        
+        if(card == ValueEnum.JOKER) {
+            for(ValueEnum value : ValueEnum.values()) {
+                if(value != ValueEnum.JOKER && value != ValueEnum.JACK) { 
+                    validDraw = isDrawAllowed(from, to, value, player);
+                    if(validDraw) return validDraw;
+                }
+            }
+        }
+
+        for (Square square : player.getOccupiedSquares()) {
+            
+            // square has token from current player
+            if (from.getName().equals(square.getName())) {
+
+                //from home to start square
+                if (from.getType() == Square.Type.HOME && to == player.getStartSquare() && 
+                        (card == ValueEnum.ACE || card == ValueEnum.KING)) {
+                    return true;
+                }
+
+                //from standart to standart square
+                if (from.getType() == Square.Type.STANDART && to.getType() == Square.Type.STANDART) {
+                    validDraw |= fromStandartToStandart(from, to, card.getI1());
+                    validDraw |= fromStandartToStandart(from, to, card.getI2());
+                    validDraw |= (card == ValueEnum.JACK) 
+                                    && to.isOccupied() 
+                                    && !player.getOccupiedSquares().contains(to);
+                    
+                    return validDraw;
+                }
+
+                //from standart to finish square
+                if (from.getType() == Square.Type.STANDART && to.getType() == Square.Type.FINISH) {
+                    for(Square finishSquare : player.getFinishSquares()){
+                        if(to.getName().equals(finishSquare.getName())){
+                            validDraw |= fromNormalToFinish(from,to,card.getI1(),player);
+                            validDraw |= fromNormalToFinish(from,to,card.getI2(),player);
+                        }
+                    }
+                } 
+            }
+        }
+
+        return validDraw;
+    }
+    
+    public static boolean fromStandartToStandart(Square from, Square to, int valueToGo) {
+        int actualValueToGo = valueToGo;
+        if(valueToGo < 0) actualValueToGo+=48;
+        int difference = Int.getDifference(from.getNumber(), to.getNumber());
+        return difference == actualValueToGo;
+    }
+
+    public static boolean fromNormalToFinish(Square from, Square to, int valueToGo, Player player) {
+        if (from != player.getStartSquare() && valueToGo > 0) {
+            int difference = Int.getDifference(from.getNumber(), player.getStartSquare().getNumber());
+            return difference + to.getNumber() + 1 == valueToGo;
+        }
+
+        return false;
+    }
+
+
+    static class Int {
+
+        private Int(){
+        }
+        
+        public static int getDifference(int i1, int i2) {
+            int diff = i2 - i1;
+            if (diff < 0) {
+                diff += 48;
+            }
+            return diff;
+        }
+
+    }
+}
---
+++ src/main/java/de/htwg/dog/model/impl/Game.java
@@ -0,0 +1,242 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.htwg.dog.model.impl;
+
+import de.htwg.dog.model.IModel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author kev
+ */
+public final class Game implements IModel {
+
+    List<Player> players;
+    Deck deck;
+    int cardsPerHand;
+    Player currentPlayer;
+    Board board;
+    String info = "";
+    Player winner;
+    
+    @Override
+    public String getInfo(){
+        return info;
+    }
+
+    public Game() {
+        Start();
+    }
+
+    void Start() {
+       board = new Board();
+       
+        players = new ArrayList<>();
+        players.add(new Player(board.getSquareByName("S0"), 0));
+        players.add(new Player(board.getSquareByName("S12"), 1));
+        players.add(new Player(board.getSquareByName("S24"), 2));
+        players.add(new Player(board.getSquareByName("S36"), 3));
+
+        for (Player player : players) {
+            board.getSquares().addAll(player.getFinishSquares());
+            board.getSquares().addAll(player.getHomeSquares());
+        }
+
+        deck = new Deck();
+        cardsPerHand = 6;
+        currentPlayer = players.get(0);
+        winner = null;
+        
+        info = "Neues Spiel gestartet.";
+
+        newRound(); 
+    }
+    
+    void newRound() {
+
+        if (cardsPerHand == 1)  
+            cardsPerHand = 6;
+        
+        if (cardsPerHand * 4 > deck.undealedCards.size()) 
+            deck = new Deck();
+
+        for (Player player : players) {
+            for (int i = 0; i < cardsPerHand; i++) {
+                Card card = deck.undealedCards.get(new Random().nextInt(deck.undealedCards.size() - 1));
+                deck.undealedCards.remove(card);
+                player.getCards().add(card);
+            }
+        }
+        cardsPerHand--;
+    }
+
+    public void nextPlayer() {
+        
+        if(!checkVictoryConditions())
+        {
+            int playerNumber = currentPlayer.getPlayerNumber();
+            Player nextPlayer;
+
+            if (playerNumber == 3) 
+                playerNumber = 0;
+            else playerNumber++;
+
+            nextPlayer = players.get(playerNumber);
+
+            while (nextPlayer.getCards().isEmpty()) {
+                if (nextPlayer == currentPlayer) {
+                    newRound();
+                    nextPlayer();
+                    return;
+                }
+
+                if (playerNumber == 3) 
+                    playerNumber = 0;
+                else 
+                    playerNumber++;
+
+                nextPlayer = players.get(playerNumber);
+            }
+
+            currentPlayer = nextPlayer;
+        }
+    }
+
+    public void setOccupiedSquares() {
+        for (Player player : players)
+            for (Square square : player.getOccupiedSquares())
+                square.setOccupation(true);
+    }
+
+    @Override
+    public Player getWinner() {
+        return winner;
+    }
+    
+    @Override
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    @Override
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    @Override
+    public void discardCards() {
+        currentPlayer.getCards().clear();
+        info = "Player " + currentPlayer.getPlayerNumber() + " hat die Karten verworfen.";
+        nextPlayer();
+    }
+    
+    @Override
+    public Square getSquare(String name) {
+        return board.getSquareByName(name);
+    }
+    
+    @Override
+    public Card getCard(String name){
+        return currentPlayer.getCardByName(name);
+    }
+
+    @Override
+    public boolean doTurn(String s1, String s2, String card) {
+        
+        if(!checkVictoryConditions())
+        {
+            if("".equals(card)){ 
+                info = "Keine Karte ausgewählt."; 
+                return false;
+            }
+
+            if("".equals(s1) || "".equals(s2)){ 
+                info = "Zu wenig Felder ausgewählt."; 
+                return false;
+            }
+
+            Square from = getSquare(s1);
+            Square to = getSquare(s2);
+            Card selectedCard = currentPlayer.getCardByName(card);
+
+            if(from == null || to == null){ 
+                info = "Unbekanntes Feld ausgewählt."; 
+                return false;
+            }
+
+            if(selectedCard == null){ 
+                info = "Unbekannte Karte ausgewählt."; 
+                return false;
+            }
+
+            if (from.isOccupied() && 
+                    Draw.isDrawAllowed(from, to, selectedCard.getValue(), currentPlayer)) {
+
+                    for(Player player : players){
+                        for(Square occupiedSquare : player.getOccupiedSquares()){
+                            if(occupiedSquare.getName().equals(to.getName())){
+                                player.getOccupiedSquares().remove(to);
+                                to.setOccupation(false);
+
+                                if(selectedCard.getValue() == ValueEnum.JACK) {
+                                    player.getOccupiedSquares().add(from);
+                                } else {
+                                    for(Square homeSquare : player.getHomeSquares()){
+                                        if(!homeSquare.isOccupied())
+                                            player.getOccupiedSquares().add(homeSquare);
+                                            homeSquare.setOccupation(true);
+                                            break;
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+
+                    currentPlayer.getOccupiedSquares().remove(from);
+                    from.setOccupation(false);
+
+                    currentPlayer.getOccupiedSquares().add(to);
+                    to.setOccupation(true);
+
+                    currentPlayer.getCards().remove(selectedCard);
+
+                    nextPlayer();
+                    info = "Der Zug war erfolgreich.";
+
+                    return true;
+                
+            }
+
+            info = "Unerlaubter Zug.";
+            return false;
+        }
+        
+        return false;
+    }
+
+    public boolean checkVictoryConditions() {
+
+        boolean hasWon = true;
+
+        for (Square square : currentPlayer.getFinishSquares())
+            hasWon &= square.isOccupied();
+
+        if(hasWon) {
+            winner = currentPlayer;
+            info = "Das Spiel ist zu Ende.";
+        }
+        
+        return hasWon;
+    }
+
+    @Override
+    public void startGame() {
+        this.Start();
+    }
+}
