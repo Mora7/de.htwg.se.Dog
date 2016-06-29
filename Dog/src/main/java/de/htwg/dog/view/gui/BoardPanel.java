45810eebf6893ae395397acc41b7b292518cb8ac
kebroger <kbroger@sunrise.ch>
2016-06-29 16:21:10 +0200

    removed unused import

--- Dog/src/main/java/de/htwg/dog/view/gui/BoardPanel.java
+++ Dog/src/main/java/de/htwg/dog/view/gui/BoardPanel.java
@@ -11,7 +11,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
